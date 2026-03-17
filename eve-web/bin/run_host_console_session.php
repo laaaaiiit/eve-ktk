ad494d097d5aa8debfe4d75a625d620e1f652569
lait <nikita_men.1@mail.ru>
2026-03-17 11:40:10 +0500

    merge script stdout and transcript for host console

--- eve-web/bin/run_host_console_session.php
+++ eve-web/bin/run_host_console_session.php
@@ -256,11 +256,31 @@ while (true) {
 
     $readOut = @fread($stdout, 65536);
     if (is_string($readOut) && $readOut !== '') {
+        $streamChunk = hostWorkerSanitizeScriptChunk($readOut);
+        if ($streamChunk !== '') {
+            $written = hostWorkerAppendOut($sessionId, $streamChunk);
+            if ($written > 0) {
+                $bytesOut += $written;
+            }
+            if (preg_match('/[^\r\n\t ]/', $streamChunk) === 1) {
+                $targetProducedOutput = true;
+            }
+        }
         $hadActivity = true;
     }
 
     $readErr = @fread($stderr, 65536);
     if (is_string($readErr) && $readErr !== '') {
+        $streamErrChunk = hostWorkerSanitizeScriptChunk($readErr);
+        if ($streamErrChunk !== '') {
+            $written = hostWorkerAppendOut($sessionId, $streamErrChunk);
+            if ($written > 0) {
+                $bytesOut += $written;
+            }
+            if (preg_match('/[^\r\n\t ]/', $streamErrChunk) === 1) {
+                $targetProducedOutput = true;
+            }
+        }
         $hadActivity = true;
     }
 
